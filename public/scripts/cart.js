98e24024947adffcb3a7f34297a24fd0b4bb85e3
OriolFilter <oriol.filter.7e3@itb.cat>
2020-05-25 21:24:24 +0200

    fiexed error: when deleting an item using the "X" the cart price wasn't updated

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -5,6 +5,7 @@ function closeButtons() {
     for (i = 0; i < closebtns.length; i++) {
         closebtns[i].addEventListener("click", function() {
             this.parentElement.style.display = 'none';
+            updateCartPrice();
         });
     }
 }
