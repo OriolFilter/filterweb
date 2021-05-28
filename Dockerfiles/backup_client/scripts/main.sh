50af24455a6b678872514dfc04d2bf9353b1a890
oriolfilter <filter.oriol@gmail.com>
2021-05-28 17:47:05 +0200

    First second backup.

--- Dockerfiles/backup_client/scripts/main.sh
+++ Dockerfiles/backup_client/scripts/main.sh
@@ -19,11 +19,11 @@ rsync -a ${V} "${ORIGINFOLDER}"/ "${HOLDERFOLDER}/${STRINGOUTPUT}"
 # Checks if the variable $SFTPHOST is configure, to decide if upload in a remote server or do the back-up locally
 if [ -z "$SFTPHOST" ] && [ -n "$DESTINATIONFOLDER" ]
 then
-      tar ${V} -czf  "${DESTINATIONFOLDER}/${STRINGOUTPUT}.tar.gz" "${HOLDERFOLDER}/${STRINGOUTPUT}"
+      tar ${V} -czfp  "${DESTINATIONFOLDER}/${STRINGOUTPUT}.tar.gz" "${HOLDERFOLDER}/${STRINGOUTPUT}"
 # Checks if all the variables required to do the backup are set up
 elif [ -n "$DESTINATIONFOLDER" ] && [ -n "$SFTPPORT" ] && [ -n "$SFTPUSER" ] && [ -n "$SFTPHOST" ] && [ -n "$SFTPDIR" ]
 then
-      tar ${V} -czf "${HOLDERFOLDER}/${STRINGOUTPUT}.tar.gz" "${HOLDERFOLDER}/${STRINGOUTPUT}"
+      tar ${V} -czfp "${HOLDERFOLDER}/${STRINGOUTPUT}.tar.gz" "${HOLDERFOLDER}/${STRINGOUTPUT}"
       sftp ${V} -P "${SFTPPORT}" -o StrictHostKeyChecking=no "${SFTPUSER}@${SFTPHOST}:${SFTPDIR}" <<< "put ${HOLDERFOLDER}/${STRINGOUTPUT}.tar.gz"
 else
 # In case of not doing anything shows this message
